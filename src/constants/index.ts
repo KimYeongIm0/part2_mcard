6d0c8faad3a581c761b4742c284119acd2636437
KimYeongIm0 <115619339+KimYeongIm0@users.noreply.github.com>
2024-06-16 20:26:24 +0900

    회원가입시 정보 파이어베이스 연결

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,6 @@
 export const COLLECTIONS = {
   CARD: "card",
   ADBANNER: "ADBANNER",
+  USER: "USER",
 } as const;
 // as const로 값을 불변으로 유지하고 오류를 방지
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,9 +1,45 @@
-import Form from "@components/signup/Form";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
-export default function Signup() {
+import Form from "@components/signup/Form";
+import { FormValues } from "@models/signup";
+
+import { auth, store } from "@remote/firebase";
+import { COLLECTIONS } from "@constants";
+
+function SignupPage() {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (formValues: FormValues) => {
+    const { email, password, name } = formValues;
+
+    const { user } = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    await updateProfile(user, {
+      displayName: name,
+    });
+
+    const newUser = {
+      uid: user.uid,
+      email: user.email,
+      displayName: name,
+    };
+
+    await setDoc(doc(collection(store, COLLECTIONS.USER), user.uid), newUser);
+
+    navigate("/");
+  };
+
   return (
     <div>
-      <Form />
+      <Form onSubmit={handleSubmit} />
     </div>
   );
 }
+
+export default SignupPage;
