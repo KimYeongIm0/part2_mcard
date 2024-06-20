35f7943f35f0c6be59afd0840d77c71a6a1462b7
KimYeongIm0 <115619339+KimYeongIm0@users.noreply.github.com>
2024-06-20 23:22:42 +0900

    동의 페이지작성

--- src/components/apply/Terms.tsx
+++ src/components/apply/Terms.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import Agreement from "../shared/Agreement";
 
 export default function Terms() {
-  return <div>Terms</div>;
+  return (
+    <div>
+      <Agreement>
+        <Agreement.Title checked={true} onChange={(e, checked) => {}}>
+          약관에 모두 동의
+        </Agreement.Title>
+        <Agreement.Description checked={false} onChange={(e, checked) => {}}>
+          약관1
+        </Agreement.Description>
+        <Agreement.Description checked={true} onChange={(e, checked) => {}}>
+          약관2
+        </Agreement.Description>
+      </Agreement>
+    </div>
+  );
 }
---
+++ src/constants/apply.ts
@@ -0,0 +1,11 @@
+export const 약관목록 = [
+  {
+    id: "01",
+    title: "카드신청 관련 안내 및 필수 동의",
+  },
+  {
+    id: "02",
+    title: "(필수) 개인정보 요약동의서",
+    link: "https://velog.io/@turret1234/React-Axios-%EC%BB%A4%EC%8A%A4%ED%85%80%EC%9C%BC%EB%A1%9C-jwt%ED%86%A0%ED%81%B0-%EC%9E%AC%EB%B0%9C%EA%B8%89",
+  },
+];
