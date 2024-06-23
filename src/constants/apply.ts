1d6abf5a74b832c704df2cc426a752db9cef24d1
KimYeongIm0 <115619339+KimYeongIm0@users.noreply.github.com>
2024-06-23 23:40:40 +0900

    카드정보신청 기본틀 작성

--- src/constants/apply.ts
+++ src/constants/apply.ts
@@ -11,3 +11,19 @@ export const 약관목록 = [
     link: "https://velog.io/@turret1234/React-Axios-%EC%BB%A4%EC%8A%A4%ED%85%80%EC%9C%BC%EB%A1%9C-jwt%ED%86%A0%ED%81%B0-%EC%9E%AC%EB%B0%9C%EA%B8%89",
   },
 ] as Term[]; //term의 배열임을 알려줌
+
+export const 연소득옵션 = [
+  { label: "600만원 ~ 5,000만원", value: "600만원 ~ 5,000만원" },
+  { label: "5,000만원 ~ 1억원", value: "5,000만원 ~ 1억원" },
+  { label: "1억원 초과", value: "1억원 초과" },
+] as Option[];
+
+export const 신용점수옵션 = [
+  { label: "600점 이상", value: "600점 이상" },
+  { label: "600점 미만", value: "600점 미만" },
+] as Option[];
+
+export const 결제일옵션 = [
+  { label: "1일", value: "1일" },
+  { label: "25일", value: "25일" },
+] as Option[];
--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -4,14 +4,20 @@ import Terms from "../components/apply/Terms";
 import BasicInfo from "../components/apply/BasicInfo";
 import CardInfo from "../components/apply/CardInfo";
 
-export default function Apply() {
-  const [step, setStep] = useState(0);
+import { ApplyValues } from "../models/apply";
 
-  const handleTermsChange = (terms: string[]) => {};
+export default function Apply() {
+  const [step, setStep] = useState(1);
+
+  const handleTermsChange = (terms: ApplyValues["terms"]) => {};
+
+  const handleBasicInfoChange = (
+    infoValues: Pick<ApplyValues, "salary" | "payDate" | "creditScore">
+  ) => {};
   return (
     <div>
       {step === 0 ? <Terms onNext={handleTermsChange} /> : null}
-      {step === 1 ? <BasicInfo /> : null}
+      {step === 1 ? <BasicInfo onNext={handleBasicInfoChange} /> : null}
       {step === 2 ? <CardInfo /> : null}
     </div>
   );
